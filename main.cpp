fca9387639caeb159f2a6ea06b78074cbba9a504
DevPauloBrito <148394940+DevPauloBrito@users.noreply.github.com>
2026-02-17 11:00:35 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct seriesTelevisivas {
+    string nome, streaming, localizacao, diretor;
+    int temporadas, episodios;
+    float nota;
+};
+
+int main(){
+
+    cout << "Teste" << endl;
+    cout << "1010"; 
+    return 0;
+
+}
+
